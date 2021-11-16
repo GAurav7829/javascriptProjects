43aa8e15c05aef813a319cdc970a498e8d5f060a
Gaurav <grvsgh7@gmail.com>
2021-11-16 21:00:09 +0530

    Breakout Game: moving ball

--- BreakoutGame/script.js
+++ BreakoutGame/script.js
@@ -49,7 +49,7 @@ for(let i=0; i<brickRowCount; i++) {
         bricks[i][j] = {x, y, ...brickInfo};
     }
 }
-console.log(bricks);
+//console.log(bricks);
 
 //draw ball on canvas
 function drawBall() {
@@ -88,15 +88,77 @@ function drawBricks(){
     });
 }
 
+//move paddle on canvas
+function movePaddle(){
+    paddle.x += paddle.dx;
+
+    //wall detection
+    if(paddle.x + paddle.w > canvas.width){
+        paddle.x = canvas.width - paddle.w;
+    }
+    if(paddle.x < 0){
+        paddle.x = 0;
+    }
+}
+
+//move ball on canvas
+function moveBall(){
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+
+    //wall collision (x - right and left)
+    if(ball.x + ball.size > canvas.width || ball.x - ball.size < 0){
+        ball.dx *= -1;
+    }
+    //wall collision (y - top and bottom)
+    if(ball.y + ball.size > canvas.height || ball.y - ball.size < 0){
+        ball.dy *= -1;
+    }
+}
+
 //draw everything
 function draw(){
+    //clear canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
     drawBall();
     drawPaddle();
     drawScore();
     drawBricks();
 }
 
-draw();
+//update canvas drawing and animation
+function update(){
+    movePaddle();
+    moveBall();
+
+    //draw everything
+    draw();
+
+    requestAnimationFrame(update);
+}
+
+update();
+
+//keydown event
+function keyDown(e){
+    if(e.key === 'Right' || e.key === 'ArrowRight'){
+        paddle.dx = paddle.speed;
+    }else if(e.key === 'Left' || e.key === 'ArrowLeft'){
+        paddle.dx = -paddle.speed;
+    }
+}
+
+//keyup event
+function keyUp(e){
+    if(e.key === 'Right' || e.key === 'ArrowRight' || e.key === 'Left' || e.key === 'ArrowLeft'){
+        paddle.dx = 0;
+    }
+}
+
+//keyboard event handlers
+document.addEventListener('keydown',keyDown);
+document.addEventListener('keyup',keyUp);
 
 //rules and close event handlers
 rulesBtn.addEventListener('click', () => {
